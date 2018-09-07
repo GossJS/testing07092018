23cb6270cf1ed189d178d36819fd7559f2b68dfe
gossoudarev <igossoudarev@herzen.spb.ru>
2018-09-07 20:35:10 +0300

    new commit

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 require ('should');
 const { get } = require('axios');
 
-const a = 0.1;
-const b = 0.2;
+const a = 1;
+const b = 2;
 const URL = `https://kodaktor.ru/api/add/${a}/${b}`;
 const headers = { 'Content-Type': 'application/json' };
 
 describe('asyncAdd', () => {
   it (`should return the sum of ${a}, ${b}`, async () => {
     const {data : {"Сумма": s}}  = await get(URL, { headers });
-    s.should.equal(0.3);
+    s.should.equal(3);
   });
 });
